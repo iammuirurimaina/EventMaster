2b8d5c699bbbaf2a6045c8cd4fcfcb3fbf2ffe3b
maina <ian.maina@student.moringaschool.com>
2023-10-05 15:00:48 +0300

    Fixed App.js

---
+++ client/src/components/App.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+
+
+import Hero from './Hero';
+import DisplayEvents from './DisplayEvents';
+function App() {
+  return (
+    <div>
+      <Hero />
+
+      
+      
+      <DisplayEvents />
+    </div>
+  );
+}
+
+export default App;
