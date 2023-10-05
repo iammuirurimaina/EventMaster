e53ccbd05cbea4fd67c01cdd038a098a9dd8208c
maina <ian.maina@student.moringaschool.com>
2023-10-05 14:08:12 +0300

    Event card displaying data correctly

---
+++ client/src/components/EventCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function EventCard({ event }) {
+  return (
+    <li key={event.id}>
+      {/* Display event information */}
+      <img src={event.image_url} alt="Event"></img>
+      <h3>{event.name}</h3>
+      <p>Date: {event.date}</p>
+      <p>Location: {event.location}</p>
+    </li>
+  );
+}
+
+export default EventCard;
