340bb827484168117dcf4b6d6455b38ac6369bad
maina <ian.maina@student.moringaschool.com>
2023-10-05 10:41:26 +0300

    Created User and Ticket Model

--- server/models.py
+++ server/models.py
@@ -3,28 +3,25 @@ from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 
 
+
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
 
 db = SQLAlchemy()
 
-# class User(db.Model, UserMixin):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
-#     password_hash = db.Column(db.String(128), nullable=False)
-#     role = db.Column(db.String(20), nullable=False)  # "admin" or "customer"
-#     #Table relationships
-#     tickets = db.relationship('Ticket', backref='user', lazy=True)
-#     cart_items = db.relationship('CartItem', backref='user', lazy=True)
-#     ticket_listings = db.relationship('TicketListing', backref='user', lazy=True)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password= db.Column(db.String(128), nullable=False)
+    role = db.Column(db.String(20), nullable=False)  # "admin" or "customer"
+    #Table relationships
+    tickets = db.relationship('Ticket', backref='user', lazy=True)
 
-#     def set_password(self, password):
-#         self.password_hash = generate_password_hash(password)
+   
 
-#     def check_password(self, password):
-#         return check_password_hash(self.password_hash, password)
+
 
 # Event Model
 class Event(db.Model):
@@ -49,10 +46,10 @@ class Event(db.Model):
         self.category = category
 
 # Ticket Model
-# class Ticket(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-#     event_id = db.Column(db.Integer, db.ForeignKey('event.id'), nullable=False)
+class Ticket(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
+    event_id = db.Column(db.Integer, db.ForeignKey('event.id'), nullable=False)
 
 # # CartItem Model
 # class CartItem(db.Model):
