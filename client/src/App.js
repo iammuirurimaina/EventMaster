497368b8054d319ac8ec8d1147677c57660ba36d
abdhata <abdhata@outlook.com>
2023-10-05 10:47:23 +0300

    Added App

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './NavBar';
+import TicketMaster from './TicketMaster';
+import AboutUs from './AboutUs';
+import LoginSignup from './LoginSignup';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <NavBar />
+        <Routes>
+          <Route path="/ticket-master" element={<TicketMaster />} />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/login-signup" element={<LoginSignup />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
