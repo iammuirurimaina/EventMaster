528999e2ab08bc4daa3414c9921fac4f7fc4afe7
maina <ian.maina@student.moringaschool.com>
2023-10-05 14:51:07 +0300

    Added Hero and SearchBar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+/* New App.js */
 
 import Hero from './Hero';
 import DisplayEvents from './DisplayEvents';
@@ -7,7 +8,7 @@ function App() {
     <div>
       <Hero />
 
-      {/* <Logo /> */}
+      
       
       <DisplayEvents />
     </div>
